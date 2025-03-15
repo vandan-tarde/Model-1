f4a0b00f69ecaff03da949b3e1cd9c96f5fd49c2
Vandant01 <tardevandan@gmail.com>
2025-03-15 23:42:58 +0530

    added the components, json files

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"J1GOg09daHjGyucLadabgTOZk3kAKzzpKj9n4WAPqdo=\"\n}"
---
+++ components/theme-provider/theme-provider.tsx
@@ -0,0 +1,8 @@
+"use client"; // Required for client-side components in Next.js 13+
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
